09c06958a735ed93e2efaefcd87680c984d1aed9
jainishank733 <jainishank733@gmail.com>
2025-08-30 14:27:57 +0530

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(5 + 5);
+
+            String name = "Alice";
+            Console.WriteLine(name);
+            Console.WriteLine($"Hello there, {name}!");
+            int mynum = 15;
+            Console.WriteLine(mynum);
+
+
+            const double pi = 3.14D, bi = 2.14D;
+            Console.WriteLine("Const var " + pi + bi);
+
+            double radius = 5.0D;
+            int ra = (int)radius;
+            Console.WriteLine("Radius " + ra);
+            Console.WriteLine(Convert.ToString(ra));
+
+
+            String s = Console.ReadLine();
+            Console.WriteLine("You entered: " + s);
+
+            Console.WriteLine(3 > 4 || 7 > 9);
+
+
+            Cons
+
+
+
+        }
+    }
+}
